c7671560e4f483213db74a3e1bdfc073ec4dc91f
Alvor <alvor1975@gmail.com>
2025-04-13 21:27:19 +0700

    Пробую свой модуль hldev

--- modules/hldevs.py
+++ modules/hldevs.py
@@ -4,10 +4,31 @@ from machine import Pin
 
 class PumpOnGPIO(Service):
     def __init__(self):
-        pass
+        super().__init__(**kwargs)
 
-    def start(self, pin, duration):
-        _p=Pin(pin,Pin.OUT)
+
+    async def start(self, pin, duration):
+        _p = Pin(pin, Pin.OUT)
+
+        # Включаем насос
         _p.value(1)
 
+        # Обновляем состояние, аналогично как в GPIO_board
+        for i in self.state['data']:
+            if i['id'] == pin and i.get("control"):
+                i['value'] = _p.value()  # Обновляем значение состояния
+                self.state['time'] = time.time()
+                asyncio.create_task(self.subscribe_handler())  # Обработчик событий (по аналогии с GPIO_board)
 
+        # Ожидаем заданную продолжительность
+        await asyncio.sleep(duration)
+
+        # Выключаем насос
+        _p.value(0)
+
+        # Снова обновляем состояние
+        for i in self.state['data']:
+            if i['id'] == pin and i.get("control"):
+                i['value'] = _p.value()  # Обновляем значение состояния
+                self.state['time'] = time.time()
+                asyncio.create_task(self.subscribe_handler())  # Обработчик событий
